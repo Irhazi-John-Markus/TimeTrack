23b82e784923bc526faef9d6c0f36b97c9c4c4a4
Irhazi-John-Markus <john.irhazi@emeal.nttdata.com>
2025-01-23 16:36:12 +0200

    added-tasks

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,23 +1,25 @@
-import { Component, Input} from '@angular/core';
-
-
+import { Component, Input,Output,computed,EventEmitter } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
 
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
-})
-export class UserComponent {
-  @Input({required:true}) avatar!: string; 
-  @Input({required:true}) name!: string;
+ })
+ export class UserComponent {
+   @Input({ required:true}) id!: string; 
+   @Input({ required:true }) avatar!: string; 
+   @Input({ required:true }) name!: string;
+   @Output() select = new EventEmitter();
+   
 
   get imagePath() {
     return '/first-angular-app/public/users' + this.avatar;
   }
 
-
-
-  onSelectUser() {}
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 
 }
