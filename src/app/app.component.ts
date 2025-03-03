186f5a28567d0d63b2608ab509792ac325dab9fe
Irhazi-John-Markus <john.irhazi@emeal.nttdata.com>
2025-03-03 21:40:35 +0200

    zoom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [
+    LeftSidebarComponent,
+    MainComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
