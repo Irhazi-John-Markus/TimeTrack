d2a0927cf62f81af9d6000684be8ddd06ce98952
Irhazi-John-Markus <john.irhazi@emeal.nttdata.com>
2025-03-04 15:15:56 +0200

    changes

--- src/app/left-sidebar/left-sidebar.component.ts
+++ src/app/left-sidebar/left-sidebar.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input, output, TrackByFunction } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-left-sidebar',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule, MatIconModule, CommonModule],
   templateUrl: './left-sidebar.component.html',
-  styleUrl: './left-sidebar.component.css'
+  styleUrl: './left-sidebar.component.css',
 })
 export class LeftSidebarComponent {
+  isLeftSidebarCollapsed = input.required<boolean>();
+  changeIsLeftSidebarCollapsed = output<boolean>();
+  items = [
+    {
+      routeLink: 'dashboard',
+      icon: 'fal fa-home',
+      label: 'Dashboard',
+    },
+    {
+      routeLink: 'products',
+      icon: 'fal fa-box-open',
+      label: 'Products',
+    },
+    {
+      routeLink: 'pages',
+      icon: 'fal fa-file',
+      label: 'Pages',
+    },
+    {
+      routeLink: 'settings',
+      icon: 'fal fa-cog',
+      label: 'Settings',
+    },
+  ];
+  trackByFn!: TrackByFunction<{ routeLink: string; icon: string; label: string; }>;
 
+  toggleCollapse(): void {
+    this.changeIsLeftSidebarCollapsed.emit(!this.isLeftSidebarCollapsed());
+  }
+
+  closeSidenav(): void {
+    this.changeIsLeftSidebarCollapsed.emit(true);
+  }
 }
