1bcbef7da3b6754eabfda15f2e5608d62831bd2e
Irhazi-John-Markus <john.irhazi@emeal.nttdata.com>
2025-04-23 15:48:51 +0300

    changed-location-file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,12 @@ const routes: Routes = [
   {
     path: 'feature1',
     loadChildren: () =>
-      import('./feature1/feature1.module').then((m) => m.Feature1Module),
+      import('../features/feature1/feature1.module').then((m) => m.Feature1Module),
   },
   {
     path: 'feature2',
     loadChildren: () =>
-      import('./feature2/feature2.module').then((m) => m.Feature2Module),
+      import('../features/feature2/feature2.module').then((m) => m.Feature2Module),
   },
   {
     path: '',
